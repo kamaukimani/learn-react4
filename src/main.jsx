7997023dce65c403e045a0ed306274f74083c7cf
kamaukimani <paulkimys285@gmail.com>
2026-06-04 21:13:42 +0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h2>welcome to my 4th react application</h2>
+    {/* <App /> */}
+  </StrictMode>,
+)
